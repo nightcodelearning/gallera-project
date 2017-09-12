6a3af204ad6919702060f492a884b9959ee1ae86
Adela Rojas <arojas@tpaga.co>
2017-09-11 21:35:37 -0500

    added model

--- gallera/api_gallera/models.py
+++ gallera/api_gallera/models.py
@@ -15,6 +15,43 @@ def get_image_path(instance, filename):
     filename = '{}.{}'.format(uuid4().hex, ext)
     return os.path.join('photos', filename)
 
+
+class Owner(ManagedTVModel):
+    """
+    Describes an Chick's Owner
+    """
+
+    ALLOWED_ID_TYPES = [('CC', 'CC'), ('CE', 'CE'), ('NIT', 'NIT'),
+                        ('TI', 'TI'), ('PA', 'PA'), ('', '')]
+
+    ID_TYPE_CC = 'CC'
+    ID_TYPE_CE = 'CE'
+    ID_TYPE_NIT = 'NIT'
+    ID_TYPE_TI = 'TI'
+    ID_TYPE_PA = 'PA'
+
+    token = models.CharField(
+        unique=True,
+        max_length=255,
+        help_text='unique identifier for owner from this system',
+        default=partial(generate_token, 'ow'),
+    )
+    legal_id_type = models.CharField(
+        max_length=4, choices=ALLOWED_ID_TYPES, blank=True, default=''
+    )
+    legal_id_number = models.CharField(max_length=20, blank=True, default='')
+    full_name = models.CharField(max_length=255, blank=True)
+    email = models.EmailField(max_length=255, blank=True)
+
+    class Meta:
+        indexes = [
+            models.Index(fields=['legal_id_number']),
+        ]
+
+    def __str__(self):
+        return self.legal_id_number
+
+
 class Chick(ManagedTVModel):
     """
     Describes a Chick stored in the application.
@@ -72,7 +109,13 @@ class Chick(ManagedTVModel):
         if self.image and hasattr(self.image, 'url'):
             return self.image.url
 
-    owner_name = models.CharField(max_length=255, blank=True)
+    owner = models.ForeignKey(
+        Owner, models.DO_NOTHING, blank=True, null=True,
+    )
+
+    @property
+    def owner_name(self):
+        return self.owner.full_name
 
     breeder_plate_number = models.CharField(max_length=255, blank=True)
     breeder_name = models.CharField(max_length=255, blank=True)
@@ -106,41 +149,5 @@ class Chick(ManagedTVModel):
 
     def __str__(self):
         return self.coliseo_plate_number
-
-
-class Owner(ManagedTVModel):
-    """
-    Describes an Chick's Owner
-    """
-
-    ALLOWED_ID_TYPES = [('CC', 'CC'), ('CE', 'CE'), ('NIT', 'NIT'),
-                        ('TI', 'TI'), ('PA', 'PA'), ('', '')]
-
-    ID_TYPE_CC = 'CC'
-    ID_TYPE_CE = 'CE'
-    ID_TYPE_NIT = 'NIT'
-    ID_TYPE_TI = 'TI'
-    ID_TYPE_PA = 'PA'
-
-    token = models.CharField(
-        unique=True,
-        max_length=255,
-        help_text='unique identifier for owner from this system',
-        default=partial(generate_token, 'ow'),
-    )
-    legal_id_type = models.CharField(
-        max_length=4, choices=ALLOWED_ID_TYPES, blank=True, default=''
-    )
-    legal_id_number = models.CharField(max_length=20, blank=True, default='')
-    full_name = models.CharField(max_length=255, blank=True)
-    email = models.EmailField(max_length=255, blank=True)
-
-    class Meta:
-        indexes = [
-            models.Index(fields=['legal_id_number']),
-        ]
-
-    def __str__(self):
-        return self.legal_id_number
 
 
